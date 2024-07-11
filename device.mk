cd0867d83b32067d5c9b92bbcc2ffc0ea71c6fa3
Debarpan102 <91316829+Debarpan102@users.noreply.github.com>
2024-07-11 23:17:49 +0530

    Done ( maybe )

--- device.mk
+++ device.mk
@@ -49,7 +49,11 @@ PRODUCT_PACKAGES += \
     qcom_decrypt_fbe
 
 # Dependencies
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion
 
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so
 
 # Screen
 TARGET_SCREEN_WIDTH := 1080
